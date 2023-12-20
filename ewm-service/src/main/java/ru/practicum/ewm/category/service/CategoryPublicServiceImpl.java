6ca0dbbc5483b16a144942a46de366bf73df74d2
Alex Zubkov <zaleksei95@yandex.ru>
2023-12-20 22:56:38 +0300

    v2

--- ewm-service/src/main/java/ru/practicum/ewm/category/service/CategoryPublicServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/ewm/category/service/CategoryPublicServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.ewm.category.dto.CategoryDto;
 import ru.practicum.ewm.category.mapper.CategoryMapper;
 import ru.practicum.ewm.category.repository.CategoryRepository;
